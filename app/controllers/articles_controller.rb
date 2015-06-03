70edd3a37621ed461fe413d461ee9ab27ef6bc9f
Jason Beaton <jasonbeaton@Jasons-MacBook-Pro.local>
2015-06-03 15:58:45 -0400

    Installed Simple Form and created show pages for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
 	def index
 	end
 
@@ -6,8 +7,11 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def show
+	end
+
 	def create
-		@article = article.new(article_params)
+		@article = Article.new(article_params)
 		if @article.save
 			redirect_to @article
 		else
@@ -16,6 +20,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :content)
